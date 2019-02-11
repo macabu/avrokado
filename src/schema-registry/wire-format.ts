b7c65b90a0c2a184ff84c344433f6a2001c9a611
Matheus Macabu <macabu.matheus@gmail.com>
2019-02-11 21:31:16 +0100

    schema-registry: add wire protocol encoder

---
+++ src/schema-registry/wire-format.ts
@@ -0,0 +1,59 @@
+export type MagicByte = number | string;
+export type SchemaId = number | string;
+export type Data = Buffer;
+
+enum BufferSize {
+  MIN = 0,
+  MAX = 8192,
+}
+
+export enum WirePosition {
+  MagicByte = 0x0,
+  SchemaId = 0x1,
+  Data = 0x5,
+}
+
+export const MAGIC_BYTE = 0x0;
+
+export const encodeWireFormat = (
+  data: Data,
+  schemaId: SchemaId,
+  magicByte: MagicByte = MAGIC_BYTE
+) => {
+  try {
+    if (!data || !data.length) {
+      throw new TypeError('Data cannot be empty');
+    }
+
+    const inputsSize =
+      WirePosition.MagicByte +
+      WirePosition.SchemaId +
+      WirePosition.Data -
+      1;
+
+    const concatBufSize = inputsSize + data.length;
+
+    if (concatBufSize <= BufferSize.MIN || concatBufSize > BufferSize.MAX) {
+      throw new TypeError('Buffer size is either too small or too big');
+    }
+
+    const saneMagicByte = (typeof magicByte === 'string')
+      ? Number.parseInt(magicByte, 16)
+      : magicByte;
+
+    const saneSchemaId = (typeof schemaId === 'string')
+      ? Number.parseInt(schemaId, 10)
+      : schemaId;
+
+    const buf = Buffer.alloc(inputsSize);
+
+    buf[0] = saneMagicByte;
+    buf.writeInt32BE(saneSchemaId, WirePosition.SchemaId);
+
+    const concatBuf = Buffer.concat([buf, data], concatBufSize);
+
+    return concatBuf;
+  } catch (error) {
+    throw error;
+  }
+};
---
+++ tests/unit/schema-registry/wire-format.test.ts
@@ -0,0 +1,67 @@
+import { encodeWireFormat } from '../../../src/schema-registry/wire-format';
+
+describe('Unit Test : src/schema-registry/wire-format.ts', () => {
+  describe('encodeWireFormat', () => {
+    test('With valid sized data and schema id',  () => {
+      expect.assertions(4);
+
+      const data = Buffer.from('A test string');
+      const schemaId = 3;
+
+      const buf = encodeWireFormat(data, schemaId);
+
+      expect(buf).toHaveLength(18);
+      expect(buf[0]).toBe(0x0);
+      expect(buf.readInt32BE(1)).toBe(schemaId);
+      expect(buf.toString()).toContain(data);
+    });
+
+    test('With valid sized data, schema id and magic byte',  () => {
+      expect.assertions(4);
+
+      const data = Buffer.from('A test string');
+      const schemaId = 3;
+      const magicByte = 0x1;
+
+      const buf = encodeWireFormat(data, schemaId, magicByte);
+
+      expect(buf).toHaveLength(18);
+      expect(buf[0]).toBe(magicByte);
+      expect(buf.readInt32BE(1)).toBe(schemaId);
+      expect(buf.toString()).toContain(data);
+    });
+
+    test('With valid sized data, schema id and magic byte as strings',  () => {
+      expect.assertions(4);
+
+      const data = Buffer.from('A test string');
+      const schemaId = '3';
+      const magicByte = '0x1';
+
+      const buf = encodeWireFormat(data, schemaId, magicByte);
+
+      expect(buf).toHaveLength(18);
+      expect(buf[0]).toBe(Number.parseInt(magicByte, 16));
+      expect(buf.readInt32BE(1)).toBe(Number.parseInt(schemaId, 10));
+      expect(buf.toString()).toContain(data);
+    });
+
+    test('With empty data',  () => {
+      const data = Buffer.alloc(0);
+      const schemaId = 3;
+
+      expect(() => {
+        encodeWireFormat(data, schemaId);
+      }).toThrow(TypeError);
+    });
+
+    test('With large data',  () => {
+      const data = Buffer.alloc(9000);
+      const schemaId = 3;
+
+      expect(() => {
+        encodeWireFormat(data, schemaId);
+      }).toThrow(TypeError);
+    });
+  });
+});
