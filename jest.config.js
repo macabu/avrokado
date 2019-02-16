62f9eac2cf607040a964a4bd3d6aed2d2bcdf653
Matheus Macabu <macabu.matheus@gmail.com>
2019-02-16 19:30:59 +0100

    chore: move jest config to package.json

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  preset: 'ts-jest/presets/js-with-ts',
-  testEnvironment: 'node',
-  collectCoverage: true,
-  coverageDirectory: './coverage/',
-  testMatch: [
-    '**/tests/**/*.test.ts',
-  ],
-}
