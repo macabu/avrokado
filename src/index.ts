af6fcc96e6bbb3b688343bc1fc022007c5ea291a
Matheus Macabu <macabu.matheus@gmail.com>
2019-03-03 14:25:02 +0100

    chore: expose producer

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { loadSchemas } from './core/load-schemas';
 export { consumerStream, KafkaMessage } from './kafka/consumer';
+export { producerStream, DEFAULT_PARTITION, produce } from './kafka/producer';
